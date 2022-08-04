d2242034cfd656a7d64c030480ec9fcb75614c99
yusufatakanozmeen <107477926+yusufatakanozmeen@users.noreply.github.com>
2022-08-04 15:43:50 +0300

    Update hipoHesaplam.java

--- hipoHesaplam.java
+++ hipoHesaplam.java
@@ -1,16 +1,49 @@
+import java.util.Scanner;
 
-        public class Urun {
-            
-            public static void main(String[] args) {
-                
-                Scanner scan = new Scanner(System.in);
-                
-                System.out.print("Ürünün KDV'siz fiyatını giriniz: ");
-                int fiyat = scan.nextInt();
-                
-                double yeni_fiyat = fiyat + (fiyat * 0.18);
-                
-                System.out.println("KDV'li fiyat = " + yeni_fiyat);
-            }
-        }
-    
+public class faizHesaplama {
+   
+    public static void main(String[] args){
+    //hipo bulma
+    /* 
+    double a,b,c;
+    Scanner oku=new Scanner(System.in);
+     
+    System.out.print("Birinci dik kenarı giriniz:");
+    a=oku.nextDouble();
+    System.out.print("İkinci dik kenarı giriniz:");
+    b=oku.nextDouble();
+     
+    c=Math.sqrt(a*a+b*b);
+     
+    System.out.printf("\nHipotenüs:%.2f",c);
+
+    */
+
+
+    /*    
+
+    //ucgenincevresi
+    double d,e,f;
+    Scanner oku=new Scanner(System.in);
+    System.out.print("Birinci dik kenarı giriniz:");
+    d=oku.nextDouble();
+    System.out.print("İkinci dik kenarı giriniz:");
+    e=oku.nextDouble(); 
+    System.out.print("üçüncü dik kenarı giriniz:");
+    f=oku.nextDouble();   
+    System.out.printf("\nÇevre:%.2f",(d+e+f));
+*/
+            /*
+    //ucgeninalani
+    double d,e,f;
+    Scanner oku=new Scanner(System.in);
+    System.out.print("Birinci dik kenarı giriniz:");
+    d=oku.nextDouble();
+    System.out.print("İkinci dik kenarı giriniz:");
+    e=oku.nextDouble();
+    System.out.print("üçüncü dik kenarı giriniz:");
+    f=oku.nextDouble();
+    System.out.printf("\nAlan:%.2f",(d*e*f)/2);
+    */
+    }
+}
